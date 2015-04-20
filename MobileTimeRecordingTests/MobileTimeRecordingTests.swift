95844052b60f95a9551549ebddfcbad5e2acd4bd
Daniel Knogl <dan.nglk@gmail.com>
2015-04-20 15:34:20 +0200

    Added new Xcode project for iphone app.

---
+++ MobileTimeRecordingTests/MobileTimeRecordingTests.swift
@@ -0,0 +1,36 @@
+//
+//  MobileTimeRecordingTests.swift
+//  MobileTimeRecordingTests
+//
+//  Created by cdan on 20/04/15.
+//  Copyright (c) 2015 develop-group. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MobileTimeRecordingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
