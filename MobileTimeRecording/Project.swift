54cf7d555704c8b6e28242db57240749072ff961
Daniel Knogl <dan.nglk@gmail.com>
2015-04-26 22:37:02 +0200

    Added new model class 'Project'. Added SQLite.swift module and dependency for having a full working SQLite wrapper for swift.

---
+++ MobileTimeRecording/Project.swift
@@ -0,0 +1,23 @@
+//
+//  Project.swift
+//  MobileTimeRecording
+//
+//  Created by cdan on 26/04/15.
+//  Copyright (c) 2015 develop-group. All rights reserved.
+//
+
+import Foundation
+import SQLite
+
+class Project
+{
+    var id: Int
+    var name: String
+    
+    
+    init(id: Int, name: String)
+    {
+        self.id = id
+        self.name = name
+    }
+}
