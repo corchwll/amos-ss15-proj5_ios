e4e200159420ebc1b90803e884c51122b5e0d7ea
Daniel Knogl <dan.nglk@gmail.com>
2015-05-08 18:28:46 +0200

    Implemented feature for displaying profile.

---
+++ MobileTimeRecording/ProfileTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileTableViewController.swift
+//  MobileTimeRecording
+//
+//  Created by cdan on 08/05/15.
+//  Copyright (c) 2015 develop-group. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewController: UITableViewController
+{
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var employeeIdLabel: UILabel!
+    @IBOutlet weak var hoursPerWeekLabel: UILabel!
+    @IBOutlet weak var vacationLabel: UILabel!
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        loadProfile()
+    }
+    
+    
+    func loadProfile()
+    {
+        let profile = profileDAO.getProfile()
+        nameLabel.text = profile.forename + " " + profile.surname
+        employeeIdLabel.text = profile.employeeId
+        hoursPerWeekLabel.text = profile.hoursPerWeek
+        vacationLabel.text = profile.vacation
+    }
+}
