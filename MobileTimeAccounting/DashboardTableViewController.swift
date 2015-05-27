ec8942a522a9e55cfe43e5028b233a3fa89dff69
Daniel Knogl <dan.nglk@gmail.com>
2015-05-28 00:00:31 +0200

    Implemented ViewController skeleton for dashboard. Updated dashboard ui to visualise overtime and vacation days.

---
+++ MobileTimeAccounting/DashboardTableViewController.swift
@@ -0,0 +1,47 @@
+/*
+    Mobile Time Accounting
+    Copyright (C) 2015
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import UIKit
+
+
+class DashboardTableViewController: UITableViewController
+{
+    @IBOutlet weak var overtimeLabel: UILabel!
+    @IBOutlet weak var vacationDaysLabel: UILabel!
+
+    
+    override func viewDidAppear(animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        
+        setUpOvertimeLabel()
+        setUpVacationDaysLabel()
+    }
+    
+    
+    func setUpOvertimeLabel()
+    {
+        overtimeLabel.text = "0"
+    }
+    
+    
+    func setUpVacationDaysLabel()
+    {
+        vacationDaysLabel.text = "0"
+    }
+}
