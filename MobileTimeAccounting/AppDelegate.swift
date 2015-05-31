d7d8593945bcf06996ac040a5573f32ef5d90b76
Daniel Knogl <dan.nglk@gmail.com>
2015-05-31 23:23:43 +0200

    Changed navigation bar style and background colour conform to the given corporate identity colours. Also did some refactoring for AppDelegate class and added missing comments.

--- MobileTimeAccounting/AppDelegate.swift
+++ MobileTimeAccounting/AppDelegate.swift
@@ -25,7 +25,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
+    /*
+        iOS function for launching application. Does some initial set up stuff, including the one time registration and navigation bar style.
+        
+        @methodtype Hook
+        @pre -
+        @post Basic application set up is fullfilled
+    */
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+    {
+        setUpOneTimeRegistration()
+        setUpNavigationBarStyle()
+    
+        return true
+    }
+    
+    
+    /*
+        Function is setting up one time registration on first startup. After registration is over, the corosponding screen will be skipped from there on.
+        
+        @methodtype Command
+        @pre -
+        @post If user not registered yet, registration will be shown
+    */
+    func setUpOneTimeRegistration()
     {
         if profileDAO.isRegistered()
         {
@@ -37,32 +60,53 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         {
             println("not yet registered")
         }
+    }
+    
+    
+    /*
+        Function is setting up navigation bar style, including bar colors.
         
-        return true
+        @methodtype Command
+        @pre -
+        @post UINavigation bar appearance is set
+    */
+    func setUpNavigationBarStyle()
+    {
+        UINavigationBar.appearance().barStyle = UIBarStyle.Black
+        UINavigationBar.appearance().translucent = false
+        UINavigationBar.appearance().barTintColor = UIColor(red: 0, green: 55/255, blue: 91/255, alpha: 255)
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    
+    func applicationWillResignActive(application: UIApplication)
+    {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    
+    func applicationDidEnterBackground(application: UIApplication)
+    {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
+    
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(application: UIApplication)
+    {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    
+    func applicationDidBecomeActive(application: UIApplication)
+    {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    
+    func applicationWillTerminate(application: UIApplication)
+    {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
