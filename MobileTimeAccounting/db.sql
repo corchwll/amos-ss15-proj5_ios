1622460a4242994f569b38176c02d5b1985be51b
Daniel Knogl <dan.nglk@gmail.com>
2015-06-19 00:00:03 +0200

    User Story #23: Changed database file by adding latitude and longitude for saving coordinates of projects.

--- MobileTimeAccounting/db.sql
+++ MobileTimeAccounting/db.sql
@@ -2,6 +2,8 @@ create table projects (
 	id text primary key, 
 	name text,
 	final_date integer,
+    latitude real,
+    longitude real,
 	is_archived boolean
 );
 
@@ -16,27 +18,35 @@ create table sessions (
 insert into projects values (
 	"00001",
 	"Vacation",
-	0,
+    0,
+    0.0,
+    0.0,
 	0
 );
 
 insert into projects values (
 	"00002",
 	"Illness",
-	0,
+    0,
+    0.0,
+    0.0,
 	0
 );
 
 insert into projects values (
 	"00003",
 	"Office",
-	0,
+    0,
+    0.0,
+    0.0,
 	0
 );
 
 insert into projects values (
     "00004",
     "Training",
-	0,
+    0,
+    0.0,
+    0.0,
     0
 );
